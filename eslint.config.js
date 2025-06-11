eeeb77d76931f7dc79539585cb60aa7171577a5c
Mandrak147 <f.medeiros.c.f@gmail.com>
2025-06-11 18:32:59 -0300

    Adiciona configuração do Prettier e integra ESLint com plugins para melhor formatação e organização do código

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,18 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import eslintImport from "eslint-plugin-import";
+import prettier from "eslint-plugin-prettier";
 
 export default tseslint.config(
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      eslintImport.configs.recommended,
+      prettier.configs.recommended,
+    ],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
@@ -17,14 +24,28 @@ export default tseslint.config(
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      import: eslintImport,
+      prettier: prettier,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-explicit-any": ["warn", { "ignoreRestArgs": true }],
+      "no-console": ["warn", { "allow": ["warn", "error"] }],
+      "no-debugger": "warn",
+      "prefer-template": "error",
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
       ],
+      "import/order": [
+        "error",
+        {
+          "alphabetize": { "order": "asc", "caseInsensitive": true },
+          "newlines-between": "always",
+          "groups": [["builtin", "external"], "internal", ["parent", "sibling", "index"]]
+        }
+      ],
+      "prettier/prettier": "error",
     },
   }
 );
